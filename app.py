4efd3731838e2d8adda2c6b2c4281b576f428ee8
izzdisini <kamvretbudjank@gmail.com>
2026-05-01 21:18:44 +0700

    app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.write("Upload gambar X-ray untuk mendapatkan prediksi.")
 # =========================
 @st.cache_resource
 def load_model():
-    model = tf.keras.models.load_model("model_parurasio801010.keras")
+    model = tf.keras.models.load_model("model_parurasio801010.h5")
 
     # optional (biar lebih aman & hilang warning)
     model.compile(
